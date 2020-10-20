48b508e37a6edf78539a0d1c4ab7a1d4881475e3
Josh Cook <joshuacook0023@gmail.com>
2020-10-20 16:10:08 -0400

    feat: specify which directory to look for notebooks to convert

--- convert_notebooks.sh
+++ convert_notebooks.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
-for ipynb in **/*.ipynb
+DIR=""
+
+if [ $# -eq 0 ]
+then
+	DIR="**"
+else
+	DIR=$1
+fi
+
+for ipynb in $DIR/*.ipynb
 do
-    jupyter nbconvert --to markdown $ipynb
+	echo $ipynb
+    # jupyter nbconvert --to markdown $ipynb
 done
