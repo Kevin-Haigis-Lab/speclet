a072c99781943abc3f7d6e09f4aeeaa795db0303
jhrcook <joshuacook0023@gmail.com>
2020-10-21 21:34:00 -0400

    feat: prepare modeling data using Snakemake workflow

--- .proj_aliases.sh
+++ .proj_aliases.sh
@@ -8,3 +8,6 @@ alias speclet_srun="srun --pty -p priority --mem 50G -c 5 -t 0-18:00 --x11 /bin/
 alias speclet_env="conda activate speclet && bash .proj_aliases.sh"
 alias speclet_jl="jupyter lab --port=7012 --browser='none'"
 alias speclet_sshlab='ssh -N -L 7012:127.0.0.1:7012'
+
+alias speclet_snakemake_env="conda activate speclet_smakemake && bash .proj_aliases.sh"
+
---
+++ munge/014_make-achilles-sample-info.R
@@ -0,0 +1,38 @@
+#!/bin/env Rscript
+
+.libPaths("/home/jc604/R-4.0/library")
+
+library(nakedpipe)
+library(tidyverse)
+
+if (basename(getwd()) == "munge") { setwd("..") }
+
+
+modeling_data_dir <- "modeling_data"
+
+
+
+mut_file_path <- file.path(modeling_data_dir, "ccle_mutations.csv")
+all_samples_with_mutation_data <- read_csv(mut_file_path, col_types = cols("chromosome" = col_character())) %>%
+    pull(depmap_id) %>%
+    unlist() %>%
+    unique()
+
+kras_mutations_path <- file.path(modeling_data_dir, "kras_mutations.csv")
+kras_mutations <- read_csv(kras_mutations_path) %>%
+    select(depmap_id, kras_mutation)
+
+    
+noncancerous_lineages <- c("unknown", "embryo")
+
+sample_info_path <- file.path(modeling_data_dir, "sample_info.csv")
+sample_info <- read_csv(sample_info_path) %.% {
+    filter(!(lineage %in% !!noncancerous_lineages))
+    filter(!str_detect(lineage, "engineer"))
+    filter(depmap_id %in% !!all_samples_with_mutation_data)
+    distinct(depmap_id, primary_or_metastasis, lineage, lineage_subtype)
+    left_join(kras_mutations, by = "depmap_id")
+    mutate(kras_mutation = ifelse(is.na(kras_mutation), "WT", kras_mutation))
+}
+
+qs::qsave(sample_info, file.path(snakemake@output[["out_file"]]))
