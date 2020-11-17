5b33b820fe33f159bb288d7944e173f3fc95773a
jhrcook <joshuacook0023@gmail.com>
2020-11-17 07:32:22 -0500

    increase srun resources requested

--- .proj_aliases.sh
+++ .proj_aliases.sh
@@ -4,7 +4,7 @@ module unload python
 module load R/4.0.1 conda2
 
 # Bash aliases used in this project.
-alias speclet_srun="srun --pty -p priority --mem 80G -c 5 -t 0-18:00 /bin/bash"
+alias speclet_srun="srun --pty -p priority --mem 150G -c 6 -t 0-15:00 /bin/bash"
 alias speclet_env="conda activate speclet && bash .proj_aliases.sh"
 alias speclet_jl="jupyter lab --port=7012 --browser='none'"
 alias speclet_sshlab='ssh -N -L 7012:127.0.0.1:7012'
