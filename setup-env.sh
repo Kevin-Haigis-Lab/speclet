f4bb58deb8d67503b91bf305841a5e6d0b90c95a
jhrcook <joshuacook0023@gmail.com>
2021-01-14 14:52:28 -0500

    chore: create a setup script and update environment.yml

---
+++ setup-env.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+conda create -n speclet python=3.8
+conda activate speclet
+
+conda config --add channels conda-forge
+
+conda install -c conda-forge --yes jupyterlab
+conda install -c conda-forge --yes pre-commit
+conda install -c conda-forge --yes jupyterlab_code_formatter
+conda install -c conda-forge --yes pymc3
+conda install -c conda-forge --yes matplotlib seaborn plotnine
+conda install -c conda-forge --yes arviz graphviz
+conda install -c conda-forge --yes black isort mypy nbqa watermark
+
+pre-commit install
+pre-commit run --all-files
