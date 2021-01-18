504de616b294d6a100ee4551f5cb75a1d6fc421b
jhrcook <joshuacook0023@gmail.com>
2021-01-18 14:40:06 -0500

    chore: env with jupyterlab-lsp

--- setup-env.sh
+++ setup-env.sh
@@ -1,17 +1,6 @@
 #!/bin/sh
 
-conda create -n speclet python=3.8
+conda create -n speclet -f environment.yml
 conda activate speclet
 
-conda config --add channels conda-forge
-
-conda install -c conda-forge --yes jupyterlab
-conda install -c conda-forge --yes pre-commit
-conda install -c conda-forge --yes jupyterlab_code_formatter
-conda install -c conda-forge --yes pymc3
-conda install -c conda-forge --yes matplotlib seaborn plotnine
-conda install -c conda-forge --yes arviz graphviz python-graphviz
-conda install -c conda-forge --yes black isort mypy nbqa watermark
-
 pre-commit install
-pre-commit run --all-files
