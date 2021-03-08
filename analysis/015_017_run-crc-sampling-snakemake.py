3a00e6da1ef93761a2571c5e03acf94a5238435a
Josh Cook <joshuacook0023@gmail.com>
2021-03-08 13:54:06 -0500

    fix: some other stupid snakemake bs

--- analysis/015_017_run-crc-sampling-snakemake.py
+++ analysis/015_017_run-crc-sampling-snakemake.py
@@ -27,7 +27,7 @@ rule report:
     params:
         model_name = lambda w: model_names[w.model]
     output:
-         ouput_notebook = lambda w: REPORTS_DIR + "/" + model_names[w.model] + ".md"
+         ouput_notebook = lambda w: REPORTS_DIR + "/{wildwards.model_name}.md"
     conda:
         "../environment.yml"
     shell:
