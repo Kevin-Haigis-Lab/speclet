6b887efad9ea7e70ef767bae06b7a1a92fdc88d6
Josh Cook <joshuacook0023@gmail.com>
2021-03-12 14:44:38 -0500

    test: test some of the CLI for sampling_pymc3_models

--- tests/test_sampling_pymc3_models.py
+++ tests/test_sampling_pymc3_models.py
@@ -7,6 +7,8 @@ import pandas as pd
 import pretty_errors
 import pymc3 as pm
 import pytest
+import typer
+from typer.testing import CliRunner
 
 from analysis import sampling_pymc3_models as sampling
 from analysis.sampling_pymc3_models import SamplingArguments
@@ -164,5 +166,35 @@ class TestCrcModel1:
             assert col in data.columns
 
 
-# TODO: test typer CLI
-#   https://typer.tiangolo.com/tutorial/testing/
+class TestTyperCLI:
+    @pytest.fixture(scope="class")
+    def app(self) -> typer.Typer:
+        app = typer.Typer()
+        app.command()(sampling.main)
+        return app
+
+    @pytest.fixture(scope="class")
+    def runner(self) -> CliRunner:
+        return CliRunner()
+
+    def test_show_help(self, app: typer.Typer, runner: CliRunner):
+        result = runner.invoke(app, ["--help"])
+        assert result.exit_code == 0
+        assert "Usage:" in result.output
+        assert "Arguments:" in result.output
+        assert "Options:" in result.output
+
+    def test_no_input_error(self, app: typer.Typer, runner: CliRunner):
+        result = runner.invoke(app, [])
+        assert "Error: Missing argument" in result.output
+        assert result.exit_code > 0
+
+    def test_not_real_model_error(self, app: typer.Typer, runner: CliRunner):
+        result = runner.invoke(app, ["fake-model"])
+        assert "Error: Invalid value" in result.output
+        assert result.exit_code > 0
+
+    def test_no_name_error(self, app: typer.Typer, runner: CliRunner):
+        result = runner.invoke(app, [sampling.ModelOption.crc_m1])
+        assert "Error: Missing argument" in result.output
+        assert result.exit_code > 0
