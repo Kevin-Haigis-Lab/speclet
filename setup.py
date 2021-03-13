a96c6e32dbc05531c5bd6b31ffafa7100d6c3cd2
Josh Cook <joshuacook0023@gmail.com>
2021-03-12 20:04:15 -0500

    style: rename package from src to specletpy

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="src",
+    name="specletpy",
     packages=find_packages(),
     version="0.1.0",
     description="Using Bayesian statistics to model CRISPR-Cas9 genetic screen data to identify, with measureable uncertainty, synthetic lethal interactions that are specific to the individual KRAS mutations.",
