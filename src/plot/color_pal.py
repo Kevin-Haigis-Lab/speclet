43a2634e1eb3f925436fa7f6720572e4ac0f843b
Josh Cook <joshuacook0023@gmail.com>
2021-03-16 16:44:06 -0400

    doc: functions in src/plot/

--- src/plot/color_pal.py
+++ src/plot/color_pal.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
-# Colors
+"""My colors and color palettes."""
 
 from enum import Enum
 
 
 class SeabornColor(str, Enum):
+    """Colors from the 'seaborn' package."""
+
     orange = "#ED854A"
     blue = "#4878D0"
     green = "#6BCC64"
