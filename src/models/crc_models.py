e785bd2e3df00d2369d8c76332e48c858e58418f
Josh Cook <joshuacook0023@gmail.com>
2021-03-19 15:29:02 -0400

    feat: move CRC Model 1 builder into a class

--- src/models/crc_models.py
+++ src/models/crc_models.py
@@ -14,7 +14,10 @@ import theano
 from theano.tensor.sharedvar import TensorSharedVariable as TTShared
 
 from src.data_processing import achilles as achelp
+from src.data_processing import common as dphelp
 from src.data_processing.common import nunique
+from src.io import data_io
+from src.models.protocols import SelfSufficientModel
 from src.models.speclet_model import SpecletModel
 
 
@@ -27,7 +30,6 @@ class CrcModel(SpecletModel):
 
     debug: bool
     data: Optional[pd.DataFrame] = None
-    data_dir: Path = Path("modeling_data")
 
     def __init__(self, cache_dir: Optional[Path] = None, debug: bool = False):
         """Create a CrcModel object.
@@ -45,12 +47,9 @@ class CrcModel(SpecletModel):
         Returns:
             Path: Path to the data.
         """
-        f = (
-            "depmap_modeling_dataframe_subsample.csv"
-            if self.debug
-            else "depmap_modeling_dataframe.csv"
-        )
-        return self.data_dir / f
+        if self.debug:
+            return data_io.data_path(to=data_io.DataFile.crc_subsample)
+        return data_io.data_path(to=data_io.DataFile.crc_data)
 
     def get_batch_size(self) -> int:
         """Decide on the minibatch size for modeling CRC data.
@@ -80,77 +79,87 @@ class CrcModel(SpecletModel):
 #### ---- Model 1 ---- ####
 
 
-def model_1(
-    sgrna_idx: np.ndarray,
-    sgrna_to_gene_idx: np.ndarray,
-    cellline_idx: np.ndarray,
-    batch_idx: np.ndarray,
-    lfc_data: np.ndarray,
-) -> Tuple[pm.Model, Dict[str, TTShared]]:
-    """Build CRC Model 1.
+class CrcModelOne(CrcModel, SelfSufficientModel):
+    """CRC Model One."""
 
-    Args:
-        sgrna_idx (np.ndarray): sgRNA index.
-        sgrna_to_gene_idx (np.ndarray): sgRNA to gene index.
-        cellline_idx (np.ndarray): Cell line index.
-        batch_idx (np.ndarray): pDNA batch index.
-        lfc_data (np.ndarray): Log-fold change (LFC) data.
+    shared_vars: Optional[Dict[str, TTShared]] = None
+    model: Optional[pm.Model] = None
 
-    Returns:
-        Tuple[pm.Model, Dict[str, TTShared]]: A collection of the model and shared variables.
-    """
-    total_size = len(lfc_data)
-    n_sgrnas = nunique(sgrna_idx)
-    n_genes = nunique(sgrna_to_gene_idx)
-    n_lines = nunique(cellline_idx)
-    n_batches = nunique(batch_idx)
+    def __init__(self, cache_dir: Optional[Path] = None, debug: bool = False):
+        """Create a CrcModelOne object.
 
-    # Shared Theano variables
-    sgrna_idx_shared = theano.shared(sgrna_idx)
-    sgrna_to_gene_idx_shared = theano.shared(sgrna_to_gene_idx)
-    cellline_idx_shared = theano.shared(cellline_idx)
-    batch_idx_shared = theano.shared(batch_idx)
-    lfc_shared = theano.shared(lfc_data)
+        Args:
+            cache_dir (Optional[Path], optional): The directory for caching sampling/fitting results. Defaults to None.
+            debug (bool, optional): Are you in debug mode? Defaults to False.
+        """
+        super().__init__(cache_dir=cache_dir, debug=debug)
 
-    with pm.Model() as model:
-        # Hyper-priors
-        μ_g = pm.Normal("μ_g", np.mean(lfc_data), 1)
-        σ_g = pm.HalfNormal("σ_g", 2)
-        σ_σ_α = pm.HalfNormal("σ_σ_α", 1)
+    def build_model(self) -> None:
+        """Build CRC Model One."""
+        data = self.get_data()
 
-        # Prior per gene that sgRNAs are sampled from.
-        μ_α = pm.Normal("μ_α", μ_g, σ_g, shape=n_genes)
-        σ_α = pm.HalfNormal("σ_α", σ_σ_α, shape=n_genes)
-        μ_β = pm.Normal("μ_β", 0, 0.2)
-        σ_β = pm.HalfNormal("σ_β", 1)
-        μ_η = pm.Normal("μ_η", 0, 0.2)
-        σ_η = pm.HalfNormal("σ_η", 1)
+        total_size = data.shape[0]
+        indices_collection = achelp.common_indices(data)
 
-        # Prior per sgRNA
-        α_s = pm.Normal(
-            "α_s",
-            μ_α[sgrna_to_gene_idx_shared],
-            σ_α[sgrna_to_gene_idx_shared],
-            shape=n_sgrnas,
-        )
-        β_l = pm.Normal("β_l", μ_β, σ_β, shape=n_lines)
-        η_b = pm.Normal("η_b", μ_η, σ_η, shape=n_batches)
+        # Shared Theano variables
+        sgrna_idx_shared = theano.shared(indices_collection.sgrna_idx)
+        sgrna_to_gene_idx_shared = theano.shared(indices_collection.sgrna_to_gene_idx)
+        cellline_idx_shared = theano.shared(indices_collection.cellline_idx)
+        batch_idx_shared = theano.shared(indices_collection.batch_idx)
+        lfc_shared = theano.shared(data.lfc.values)
 
-        # Main model level
-        μ = pm.Deterministic(
-            "μ",
-            α_s[sgrna_idx_shared] + β_l[cellline_idx_shared] + η_b[batch_idx_shared],
-        )
-        σ = pm.HalfNormal("σ", 2)
+        with pm.Model() as model:
+            # Hyper-priors
+            μ_g = pm.Normal("μ_g", np.mean(data.lfc.values), 1)
+            σ_g = pm.HalfNormal("σ_g", 2)
+            σ_σ_α = pm.HalfNormal("σ_σ_α", 1)
 
-        # Likelihood
-        lfc = pm.Normal("lfc", μ, σ, observed=lfc_shared, total_size=total_size)
+            # Prior per gene that sgRNAs are sampled from.
+            μ_α = pm.Normal("μ_α", μ_g, σ_g, shape=indices_collection.n_genes)
+            σ_α = pm.HalfNormal("σ_α", σ_σ_α, shape=indices_collection.n_genes)
+            μ_β = pm.Normal("μ_β", 0, 0.2)
+            σ_β = pm.HalfNormal("σ_β", 1)
+            μ_η = pm.Normal("μ_η", 0, 0.2)
+            σ_η = pm.HalfNormal("σ_η", 1)
 
-    shared_vars = {
-        "sgrna_idx_shared": sgrna_idx_shared,
-        "sgrna_to_gene_idx_shared": sgrna_to_gene_idx_shared,
-        "cellline_idx_shared": cellline_idx_shared,
-        "batch_idx_shared": batch_idx_shared,
-        "lfc_shared": lfc_shared,
-    }
-    return model, shared_vars
+            # Prior per sgRNA
+            α_s = pm.Normal(
+                "α_s",
+                μ_α[sgrna_to_gene_idx_shared],
+                σ_α[sgrna_to_gene_idx_shared],
+                shape=indices_collection.n_sgrnas,
+            )
+            β_l = pm.Normal("β_l", μ_β, σ_β, shape=indices_collection.n_celllines)
+            η_b = pm.Normal("η_b", μ_η, σ_η, shape=indices_collection.n_batches)
+
+            # Main model level
+            μ = pm.Deterministic(
+                "μ",
+                α_s[sgrna_idx_shared]
+                + β_l[cellline_idx_shared]
+                + η_b[batch_idx_shared],
+            )
+            σ = pm.HalfNormal("σ", 2)
+
+            # Likelihood
+            lfc = pm.Normal("lfc", μ, σ, observed=lfc_shared, total_size=total_size)
+
+        shared_vars = {
+            "sgrna_idx_shared": sgrna_idx_shared,
+            "sgrna_to_gene_idx_shared": sgrna_to_gene_idx_shared,
+            "cellline_idx_shared": cellline_idx_shared,
+            "batch_idx_shared": batch_idx_shared,
+            "lfc_shared": lfc_shared,
+        }
+
+        self.model = model
+        self.shared_vars = shared_vars
+        return None
+
+    def sample_model(self):
+        """Sample from the model."""
+        print("sampling...")
+
+    def run_simulation_based_calibration(self):
+        """Run a round of simulation-based calibration."""
+        print("Running SBC...")
