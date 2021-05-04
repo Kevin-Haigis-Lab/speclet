5898deb1f9ff9a0eacb9f3e6431e95022cb39323
Josh Cook <joshuacook0023@gmail.com>
2021-05-04 10:03:07 -0400

    feat: ArvizCacheManager w/ tests

--- src/managers/cache_managers.py
+++ src/managers/cache_managers.py
@@ -2,24 +2,50 @@
 
 """PyMC3 model cache manager."""
 
-import os
 import pickle
+import shutil
 from pathlib import Path
-from typing import Any, Union
+from typing import Any, Optional, Tuple, Union
 
+import arviz as az
 import pymc3 as pm
 from pydantic import BaseModel
 
+from src.loggers import logger
 from src.modeling import pymc3_sampling_api as pmapi
 
 
-class ModelCachePaths(BaseModel):
-    """Paths for model caching."""
+class ModelComponentCachePaths(BaseModel):
+    """Paths for caching model components."""
 
     trace_path: Path
     prior_predictive_path: Path
     posterior_predictive_path: Path
     approximation_path: Path
+
+
+class ArvizCachePaths(BaseModel):
+    """Paths for caching ArviZ data."""
+
+    inference_data_path: Path
+    approximation_path: Path
+
+
+def _mkdir(dir: Path):
+    if not dir.exists():
+        dir.mkdir(parents=True)
+
+
+def _write_pickle(x: Any, fp: Path) -> None:
+    with open(fp, "wb") as f:
+        pickle.dump(x, f)
+    return None
+
+
+def _get_pickle(fp: Path) -> Any:
+    with open(fp, "rb") as f:
+        d = pickle.load(f)
+    return d
 
 
 class Pymc3CacheManager:
@@ -28,27 +54,22 @@ class Pymc3CacheManager:
     cache_dir: Path
 
     def __init__(self, cache_dir: Path):
-        """Instantiate a new SpecletModel object.
+        """Instantiate a new Pymc3CacheManager object.
 
         Args:
             cache_dir (Path): The directory for caching sampling/fitting results.
-              Defaults to None.
         """
         self.cache_dir = cache_dir
-        self._mkdir()
+        _mkdir(self.cache_dir)
 
-    def _mkdir(self):
-        if not self.cache_dir.exists():
-            self.cache_dir.mkdir(parents=True)
-
-    def get_cache_file_names(self) -> ModelCachePaths:
+    def get_cache_file_names(self) -> ModelComponentCachePaths:
         """Generate standard caching file paths.
 
         Raises:
             ValueError: Thrown if `self.cache_dir` is None.
 
         Returns:
-            ModelCachePaths: Object containing the paths to use for caching
+            ModelComponentCachePaths: Object containing the paths to use for caching
               various results from fitting a model.
         """
         if self.cache_dir is None:
@@ -59,7 +80,7 @@ class Pymc3CacheManager:
         prior_file_path = self.cache_dir / "prior-predictive-check.pkl"
         post_file_path = self.cache_dir / "posterior-predictive-check.pkl"
         approx_file_path = self.cache_dir / "vi-approximation.pkl"
-        return ModelCachePaths(
+        return ModelComponentCachePaths(
             trace_path=trace_dir_path,
             prior_predictive_path=prior_file_path,
             posterior_predictive_path=post_file_path,
@@ -78,8 +99,8 @@ class Pymc3CacheManager:
         cache_paths = self.get_cache_file_names()
 
         trace = pm.load_trace(cache_paths.trace_path.as_posix(), model=model)
-        post_check = self._get_pickle(cache_paths.posterior_predictive_path)
-        prior_check = self._get_pickle(cache_paths.prior_predictive_path)
+        post_check = _get_pickle(cache_paths.posterior_predictive_path)
+        prior_check = _get_pickle(cache_paths.prior_predictive_path)
 
         return pmapi.MCMCSamplingResults(
             trace=trace, prior_predictive=prior_check, posterior_predictive=post_check
@@ -98,9 +119,9 @@ class Pymc3CacheManager:
         """
         cache_paths = self.get_cache_file_names()
 
-        post_check = self._get_pickle(cache_paths.posterior_predictive_path)
-        prior_check = self._get_pickle(cache_paths.prior_predictive_path)
-        approx = self._get_pickle(cache_paths.approximation_path)
+        post_check = _get_pickle(cache_paths.posterior_predictive_path)
+        prior_check = _get_pickle(cache_paths.prior_predictive_path)
+        approx = _get_pickle(cache_paths.approximation_path)
         trace = approx.sample(draws)
 
         return pmapi.ApproximationSamplingResults(
@@ -119,14 +140,12 @@ class Pymc3CacheManager:
             res (Union[pmapi.MCMCSamplingResults, pmapi.ApproximationSamplingResults]):
               The results to cache.
         """
-        self._mkdir()
+        _mkdir(self.cache_dir)
         cache_paths = self.get_cache_file_names()
-        self._write_pickle(
-            res.posterior_predictive, cache_paths.posterior_predictive_path
-        )
-        self._write_pickle(res.prior_predictive, cache_paths.prior_predictive_path)
+        _write_pickle(res.posterior_predictive, cache_paths.posterior_predictive_path)
+        _write_pickle(res.prior_predictive, cache_paths.prior_predictive_path)
         if isinstance(res, pmapi.ApproximationSamplingResults):
-            self._write_pickle(res.approximation, cache_paths.approximation_path)
+            _write_pickle(res.approximation, cache_paths.approximation_path)
         elif isinstance(res, pmapi.MCMCSamplingResults):
             pm.save_trace(
                 res.trace, directory=cache_paths.trace_path.as_posix(), overwrite=True
@@ -160,16 +179,6 @@ class Pymc3CacheManager:
         else:
             raise ValueError(f"Unknown method '{method}'.")
 
-    def _write_pickle(self, x: Any, fp: Path) -> None:
-        with open(fp, "wb") as f:
-            pickle.dump(x, f)
-        return None
-
-    def _get_pickle(self, fp: Path) -> Any:
-        with open(fp, "rb") as f:
-            d = pickle.load(f)
-        return d
-
     def clear_cache(self) -> bool:
         """Remove the cache directory.
 
@@ -178,6 +187,154 @@ class Pymc3CacheManager:
               directory did not exist.
         """
         if self.cache_dir.exists():
-            os.rmdir(self.cache_dir)
+            shutil.rmtree(self.cache_dir)
+            return True
+        return False
+
+
+class ArvizCacheManager:
+    """Cache of model results using ArviZ's InferenceData object."""
+
+    cache_dir: Path
+
+    def __init__(self, cache_dir: Path):
+        """Instantiate a new ArvizCacheManager object.
+
+        Args:
+            cache_dir (Path): The directory for caching sampling/fitting results.
+        """
+        self.cache_dir = cache_dir
+        _mkdir(self.cache_dir)
+
+    def get_cache_file_names(self) -> ArvizCachePaths:
+        """Generate standard caching file paths.
+
+        Raises:
+            ValueError: Thrown if `self.cache_dir` is None.
+
+        Returns:
+            ArvizCachePaths: Object containing the paths to use for caching the ArviZ
+              InferenceObject and approximation object.
+        """
+        if self.cache_dir is None:
+            raise ValueError(
+                "Cannot generate caching directory when `cache_dir` is None."
+            )
+        inference_data_file_path = self.cache_dir / "inference-data.netcdf"
+        approx_file_path = self.cache_dir / "vi-approximation.pkl"
+        return ArvizCachePaths(
+            inference_data_path=inference_data_file_path,
+            approximation_path=approx_file_path,
+        )
+
+    def cache_sampling_results(
+        self,
+        inference_data: az.InferenceData,
+        approximation: Optional[pm.Approximation] = None,
+    ) -> None:
+        """Cache sampling results to disk.
+
+        Args:
+            inference_data (az.InferenceData): ArviZ InferenceData with sampling
+              results.
+            approximation (Optional[pm.Approximation], optional): Approximation results
+              for VI methods. Defaults to None.
+        """
+        _mkdir(self.cache_dir)
+        cache_paths = self.get_cache_file_names()
+        logger.info(
+            f"Caching InferenceData to '{cache_paths.inference_data_path.as_posix()}'."
+        )
+        inference_data.to_netcdf(filename=cache_paths.inference_data_path.as_posix())
+        if approximation is not None:
+            logger.info(
+                f"Caching approx. to '{cache_paths.approximation_path.as_posix()}'."
+            )
+            _write_pickle(approximation, cache_paths.approximation_path)
+
+    def cache_exists(self, method: str) -> bool:
+        """Confirm that the cached sampling/fitting results exist.
+
+        Args:
+            method (str): Which cache to look for (either "mcmc" or "approx").
+
+        Raises:
+            ValueError: If the passed method is not an acceptable option.
+
+        Returns:
+            bool: Does the cache exist?
+        """
+        cache_paths = self.get_cache_file_names()
+        if not cache_paths.inference_data_path.exists():
+            return False
+
+        if method == "mcmc":
+            # Nothing implemented at the moment.
+            logger.info("ArvizCacheManager: MCMC cache exists.")
+            return True
+        elif method == "approx":
+            if cache_paths.approximation_path.exists():
+                logger.info("ArvizCacheManager: ADVI cache exists.")
+                return True
+            return False
+        else:
+            raise ValueError(f"Unknown method '{method}'.")
+
+    def read_cached_sampling(self, check_exists: bool = True) -> az.InferenceData:
+        """Read sampling from cache.
+
+        Args:
+            check_exists (bool, optional): Should the existence of the cache be checked
+              first? Defaults to True.
+
+        Raises:
+            FileNotFoundError: If the cache does not exists (only if
+            `check_exists = True`).
+
+        Returns:
+            az.InferenceData: The cached data.
+        """
+        logger.debug("Reading sampling cache from file.")
+        cache_paths = self.get_cache_file_names()
+        if check_exists:
+            if not self.cache_exists(method="mcmc"):
+                raise FileNotFoundError("Cannot locate cached data.")
+        return az.InferenceData.from_netcdf(cache_paths.inference_data_path.as_posix())
+
+    def read_cached_approximation(
+        self, check_exists: bool = True
+    ) -> Tuple[az.InferenceData, pm.Approximation]:
+        """Read VI Approximation results from cache.
+
+        Args:
+            check_exists (bool, optional): Should the existence of the cache be checked
+              first? Defaults to True.
+
+        Raises:
+            FileNotFoundError: If the cache does not exists (only if
+            `check_exists = True`).
+
+        Returns:
+            Tuple[az.InferenceData, pm.Approximation]: The cached data.
+        """
+        logger.debug("Reading approximation cache from file.")
+        cache_paths = self.get_cache_file_names()
+        if check_exists:
+            if not self.cache_exists(method="approx"):
+                raise FileNotFoundError("Cannot locate cached data.")
+        inf_data = self.read_cached_sampling(check_exists=False)
+        approx = _get_pickle(cache_paths.approximation_path)
+        return inf_data, approx
+
+    def clear_cache(self) -> bool:
+        """Remove the cache directory.
+
+        Returns:
+            bool: Whether the directory was removed or not. Returns False if the
+              directory did not exist.
+        """
+        logger.debug("Trying to clear cache.")
+        if self.cache_dir.exists():
+            shutil.rmtree(self.cache_dir)
             return True
         return False
---
+++ tests/managers/test_cache_managers.py
@@ -0,0 +1,293 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+
+import arviz as az
+import numpy as np
+import pandas as pd
+import pymc3 as pm
+import pytest
+
+from src.managers.cache_managers import ArvizCacheManager, Pymc3CacheManager
+from src.modeling import pymc3_sampling_api as pmapi
+
+
+@pytest.fixture(scope="module")
+def data() -> pd.DataFrame:
+    real_a = 1
+    real_b = 2
+    x = np.random.uniform(-10, 10, 100)
+    real_sigma = 0.1
+    y = real_a + real_b * x + np.random.normal(0, real_sigma, len(x))
+    return pd.DataFrame({"x": x, "y_obs": y})
+
+
+@pytest.fixture(scope="module")
+def pm_model(data: pd.DataFrame) -> pm.Model:
+    with pm.Model() as model:
+        a = pm.Normal("a", 0, 5)
+        b = pm.Normal("b", 0, 1)
+        mu = a + b * data.x.values
+        sigma = pm.HalfNormal("sigma", 1)
+        y = pm.Normal("y", mu, sigma, observed=data.y_obs.values)  # noqa: F841
+    return model
+
+
+@pytest.fixture(scope="module")
+def mcmc_results(pm_model: pm.Model) -> pmapi.MCMCSamplingResults:
+    with pm_model:
+        prior = pm.sample_prior_predictive(samples=100, random_seed=123)
+        trace = pm.sample(
+            1000,
+            tune=1000,
+            cores=2,
+            chains=2,
+            random_seed=123,
+            return_inferencedata=False,
+        )
+        post = pm.sample_posterior_predictive(trace=trace, samples=100, random_seed=123)
+    return pmapi.MCMCSamplingResults(
+        trace=trace, prior_predictive=prior, posterior_predictive=post
+    )
+
+
+@pytest.fixture(scope="module")
+def advi_results(pm_model: pm.Model) -> pmapi.ApproximationSamplingResults:
+    with pm_model:
+        prior = pm.sample_prior_predictive(samples=100, random_seed=123)
+        approx = pm.fit(
+            100000,
+            callbacks=[pm.callbacks.CheckParametersConvergence(tolerance=1e-4)],
+            random_seed=123,
+        )
+        trace = approx.sample(draws=1000)
+        post = pm.sample_posterior_predictive(trace=trace, samples=100, random_seed=123)
+    return pmapi.ApproximationSamplingResults(
+        trace=trace,
+        prior_predictive=prior,
+        posterior_predictive=post,
+        approximation=approx,
+    )
+
+
+class TestPymc3CacheManager:
+    def test_cache_dir_is_made(self, tmp_path: Path):
+        cache_dir = tmp_path / "pymc3-cache-dir"
+        assert not cache_dir.exists()
+        _ = Pymc3CacheManager(cache_dir=tmp_path / cache_dir)
+        assert cache_dir.exists()
+
+    def test_generation_of_cache_paths(self, tmp_path: Path):
+        cm = Pymc3CacheManager(cache_dir=tmp_path)
+        cache_paths = cm.get_cache_file_names()
+
+        for path in cache_paths.dict().values():
+            assert tmp_path == path.parent
+
+        assert "prior" in cache_paths.prior_predictive_path.name
+        assert "posterior" in cache_paths.posterior_predictive_path.name
+        assert "trace" in cache_paths.trace_path.name
+        assert "approx" in cache_paths.approximation_path.name
+
+    @pytest.mark.slow
+    def test_caching_mcmc(
+        self, tmp_path: Path, mcmc_results: pmapi.MCMCSamplingResults
+    ):
+        cm = Pymc3CacheManager(cache_dir=tmp_path)
+        cm.cache_sampling_results(res=mcmc_results)
+        cache_paths = cm.get_cache_file_names()
+        assert (
+            cache_paths.prior_predictive_path.exists()
+            and cache_paths.prior_predictive_path.is_file()
+        )
+        assert (
+            cache_paths.posterior_predictive_path.exists()
+            and cache_paths.posterior_predictive_path.is_file()
+        )
+        assert cache_paths.trace_path.exists() and cache_paths.trace_path.is_dir()
+        assert not cache_paths.approximation_path.exists()
+
+    @pytest.mark.slow
+    def test_caching_advi(
+        self, tmp_path: Path, advi_results: pmapi.ApproximationSamplingResults
+    ):
+        cm = Pymc3CacheManager(cache_dir=tmp_path)
+        cm.cache_sampling_results(res=advi_results)
+        cache_paths = cm.get_cache_file_names()
+        assert (
+            cache_paths.prior_predictive_path.exists()
+            and cache_paths.prior_predictive_path.is_file()
+        )
+        assert (
+            cache_paths.posterior_predictive_path.exists()
+            and cache_paths.posterior_predictive_path.is_file()
+        )
+        assert (
+            cache_paths.approximation_path.exists()
+            and cache_paths.approximation_path.is_file()
+        )
+        assert not cache_paths.trace_path.exists()
+
+    @pytest.mark.slow
+    def test_read_mcmc_cache(
+        self,
+        tmp_path: Path,
+        pm_model: pm.Model,
+        mcmc_results: pmapi.MCMCSamplingResults,
+    ):
+        cm = Pymc3CacheManager(cache_dir=tmp_path)
+        cm.cache_sampling_results(res=mcmc_results)
+        cached_res = cm.read_cached_sampling(model=pm_model)
+        assert isinstance(cached_res, pmapi.MCMCSamplingResults)
+        for p in ["a", "b", "sigma"]:
+            np.testing.assert_array_equal(
+                mcmc_results.prior_predictive[p], cached_res.prior_predictive[p]
+            )
+            np.testing.assert_array_equal(mcmc_results.trace[p], cached_res.trace[p])
+
+        np.testing.assert_array_equal(
+            mcmc_results.prior_predictive["y"], cached_res.prior_predictive["y"]
+        )
+        np.testing.assert_array_equal(
+            mcmc_results.posterior_predictive["y"], cached_res.posterior_predictive["y"]
+        )
+
+    @pytest.mark.slow
+    def test_read_advi_cache(
+        self, tmp_path: Path, advi_results: pmapi.ApproximationSamplingResults
+    ):
+        cm = Pymc3CacheManager(cache_dir=tmp_path)
+        cm.cache_sampling_results(res=advi_results)
+        cached_res = cm.read_cached_approximation(draws=1000)
+        assert isinstance(cached_res, pmapi.ApproximationSamplingResults)
+        for p in ["a", "b", "sigma"]:
+            np.testing.assert_array_equal(
+                advi_results.prior_predictive[p], cached_res.prior_predictive[p]
+            )
+
+        np.testing.assert_array_equal(
+            advi_results.approximation.hist, cached_res.approximation.hist
+        )
+        np.testing.assert_array_equal(
+            advi_results.prior_predictive["y"], cached_res.prior_predictive["y"]
+        )
+        np.testing.assert_array_equal(
+            advi_results.posterior_predictive["y"], cached_res.posterior_predictive["y"]
+        )
+
+        assert cached_res.trace["a"].shape[0] == 1000
+        cached_res = cm.read_cached_approximation(draws=583)
+        assert cached_res.trace["a"].shape[0] == 583
+
+
+@pytest.mark.DEV
+class TestArvizCacheManager:
+    @pytest.fixture(scope="class")
+    def mcmc_inf_data(
+        self, pm_model: pm.Model, mcmc_results: pmapi.MCMCSamplingResults
+    ) -> az.InferenceData:
+        return az.from_pymc3(
+            trace=mcmc_results.trace,
+            prior=mcmc_results.prior_predictive,
+            posterior_predictive=mcmc_results.posterior_predictive,
+            model=pm_model,
+        )
+
+    @pytest.fixture(scope="class")
+    def advi_inf_data(
+        self, pm_model: pm.Model, advi_results: pmapi.ApproximationSamplingResults
+    ) -> az.InferenceData:
+        return az.from_pymc3(
+            trace=advi_results.trace,
+            prior=advi_results.prior_predictive,
+            posterior_predictive=advi_results.posterior_predictive,
+            model=pm_model,
+        )
+
+    def test_cache_dir_is_made(self, tmp_path: Path):
+        cache_dir = tmp_path / "pymc3-cache-dir"
+        assert not cache_dir.exists()
+        _ = ArvizCacheManager(cache_dir=tmp_path / cache_dir)
+        assert cache_dir.exists()
+
+    def test_generation_of_cache_paths(self, tmp_path: Path):
+        cm = ArvizCacheManager(cache_dir=tmp_path)
+        cache_paths = cm.get_cache_file_names()
+
+        for path in cache_paths.dict().values():
+            assert tmp_path == path.parent
+
+    @pytest.mark.slow
+    def test_caching_mcmc(self, tmp_path: Path, mcmc_inf_data: az.InferenceData):
+        cm = ArvizCacheManager(cache_dir=tmp_path)
+        cm.cache_sampling_results(mcmc_inf_data)
+        cache_paths = cm.get_cache_file_names()
+        assert (
+            cache_paths.inference_data_path.exists()
+            and cache_paths.inference_data_path.is_file()
+        )
+        assert not cache_paths.approximation_path.exists()
+
+    @pytest.mark.slow
+    def test_caching_advi(
+        self,
+        tmp_path: Path,
+        advi_inf_data: az.InferenceData,
+        advi_results: pmapi.ApproximationSamplingResults,
+    ):
+        cm = ArvizCacheManager(cache_dir=tmp_path)
+        cm.cache_sampling_results(
+            advi_inf_data, approximation=advi_results.approximation
+        )
+        cache_paths = cm.get_cache_file_names()
+        assert (
+            cache_paths.inference_data_path.exists()
+            and cache_paths.inference_data_path.is_file()
+        )
+        assert (
+            cache_paths.approximation_path.exists()
+            and cache_paths.approximation_path.is_file()
+        )
+
+    @staticmethod
+    def compare_inference_datas(id1: az.InferenceData, id2: az.InferenceData):
+        for p in ["a", "b", "sigma"]:
+            np.testing.assert_array_equal(id1.prior[p], id2.prior[p])
+            np.testing.assert_array_equal(id1.posterior[p], id2.posterior[p])
+
+        np.testing.assert_array_equal(
+            id1.prior_predictive["y"], id2.prior_predictive["y"]
+        )
+        np.testing.assert_array_equal(
+            id1.posterior_predictive["y"],
+            id2.posterior_predictive["y"],
+        )
+
+    @pytest.mark.slow
+    def test_read_mcmc_cache(
+        self,
+        tmp_path: Path,
+        pm_model: pm.Model,
+        mcmc_inf_data: az.InferenceData,
+    ):
+        cm = ArvizCacheManager(cache_dir=tmp_path)
+        cm.cache_sampling_results(mcmc_inf_data)
+        cached_res = cm.read_cached_sampling()
+        assert isinstance(cached_res, az.InferenceData)
+        TestArvizCacheManager.compare_inference_datas(mcmc_inf_data, cached_res)
+
+    @pytest.mark.slow
+    def test_read_advi_cache(
+        self,
+        tmp_path: Path,
+        advi_inf_data: az.InferenceData,
+        advi_results: pmapi.ApproximationSamplingResults,
+    ):
+        cm = ArvizCacheManager(cache_dir=tmp_path)
+        cm.cache_sampling_results(
+            advi_inf_data, approximation=advi_results.approximation
+        )
+        cached_res, approx = cm.read_cached_approximation()
+        assert isinstance(cached_res, az.InferenceData)
+        assert isinstance(approx, pm.Approximation)
+        TestArvizCacheManager.compare_inference_datas(advi_inf_data, cached_res)
