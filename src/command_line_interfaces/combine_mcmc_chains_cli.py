8c2e483d1cfe05d0a29d1da2652f265ceed18786
Josh Cook <joshuacook0023@gmail.com>
2021-05-27 06:32:56 -0400

    doc: add more logging to combine MCMC CLI

--- src/command_line_interfaces/combine_mcmc_chains_cli.py
+++ src/command_line_interfaces/combine_mcmc_chains_cli.py
@@ -44,30 +44,46 @@ def combine_mcmc_chains(
     name = cli_helpers.clean_model_names(name)
 
     n_chains = len(chains)
-    logger.info(f"Found {n_chains} separate chains.")
+    logger.info(f"Expecting {n_chains} separate chains.")
 
     ModelClass = cli_helpers.get_model_class(model_opt=model)
     logger.info(f"Detected model: '{ModelClass.__name__}'")
 
     model_names = _get_model_names_for_separate_chains(name, n_chains)
-    logger.debug("Expecting the following model names: {model_names}")
+    logger.debug(f"Expecting the following model names: {model_names}")
 
     all_arviz_data_objects: List[az.InferenceData] = []
     for chain_name in model_names:
+        logger.info(f"Gather MCMC chain '{chain_name}'.")
         sp_model = ModelClass(
             name=chain_name, root_cache_dir=cache_io.default_cache_dir(), debug=debug
         )
+
+        logger.debug(f"Cache directory: {sp_model.cache_manager.cache_dir.as_posix()}")
+        logger.debug(
+            f"Cache directory exists: {sp_model.cache_manager.cache_dir.exists()}"
+        )
+        _mcmc_cache_dir = sp_model.cache_manager.mcmc_cache_delegate.cache_dir
+        logger.debug(f"MCMC cache directory: {_mcmc_cache_dir.as_posix()}")
+        logger.debug(f"MCMC cache directory exists: {_mcmc_cache_dir.exists()}")
+
         cli_helpers.modify_model_by_name(model=sp_model, name=name)
         if sp_model.cache_manager.mcmc_cache_exists():
+            logger.info(f"Found chain '{chain_name}'.")
             sp_model.build_model()
             assert sp_model.model is not None
             mcmc_res = sp_model.mcmc_sample_model()
             all_arviz_data_objects.append(mcmc_res)
         else:
-            logger.warning(
-                f"Cache for SpecletModel '{sp_model.name}' does not exists - skipping."
+            logger.error(
+                f"Cache for SpecletModel '{sp_model.name}' does not exist - skipping."
             )
-    logger.info(f"Collected {len(all_arviz_data_objects)} chains.")
+
+    if len(all_arviz_data_objects) != n_chains:
+        logger.error(f"Expected {n_chains} but found {len(all_arviz_data_objects)}.")
+    else:
+        logger.info(f"Collected {len(all_arviz_data_objects)} chains.")
+
     combined_chains = az.concat(all_arviz_data_objects, dim="chain")
     combined_sp_model = ModelClass(
         name=name, root_cache_dir=cache_io.default_cache_dir(), debug=debug
@@ -78,9 +94,13 @@ def combine_mcmc_chains(
         logger.error(f"`combined_chains` of type '{type(combined_chains)}'")
         raise TypeError("`combined_chains` was not of type 'arviz.InferenceData'.")
     combined_sp_model.write_mcmc_cache()
+
     logger.info(f"Finished combining chains for model '{combined_sp_model.name}'")
+
     if touch_file is not None:
+        logger.info(f"Touching file: '{touch_file.as_posix()}'.")
         touch_file.touch()
+
     return None
 
 
