528a9f039b61b88f8548e05a41ec9c3a3abeefdb
Josh Cook <joshuacook0023@gmail.com>
2021-06-02 11:00:38 -0400

    feat: munge snakemake to run on O2

--- munge/munge.sh
+++ munge/munge.sh
@@ -3,7 +3,7 @@
 #SBATCH -c 2
 #SBATCH -p priority
 #SBATCH -t 1-00:00
-#SBATCH --mem 8G
+#SBATCH --mem 2G
 #SBATCH -o logs/%j_munge-pipeline.log
 #SBATCH -e logs/%j_munge-pipeline.log
 
@@ -19,22 +19,21 @@ conda activate speclet_snakemake
 
 SNAKEFILE="munge/munge.smk"
 
-snakemake \
-    --snakefile $SNAKEFILE \
-    --jobs 2 \
-    --restart-times 0 \
-    --latency-wait 120 \
-    --keep-going
-
 # snakemake \
 #     --snakefile $SNAKEFILE \
-#     --jobs 20 \
+#     --jobs 2 \
 #     --restart-times 0 \
 #     --latency-wait 120 \
-#     --use-conda \
-#     --drmaa " -c {cluster.cores} -p {cluster.partition} --mem={cluster.mem} -t {cluster.time} -o {cluster.out} -e {cluster.err} -J {cluster.J}" \
-#     --cluster-config pipelines/010_011_smk-config.json \
 #     --keep-going
 
+snakemake \
+    --snakefile $SNAKEFILE \
+    --jobs 9000 \
+    --restart-times 0 \
+    --latency-wait 120 \
+    --drmaa " -c {cluster.cores} -p {cluster.partition} --mem={cluster.mem} -t {cluster.time} -o {cluster.out} -e {cluster.err} -J {cluster.J}" \
+    --cluster-config munge/munge-config.json \
+    --keep-going
 
 conda deactivate
+exit 1
