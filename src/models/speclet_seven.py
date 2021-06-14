95a7628b8eeab07c49ece36754a2813e0520d0e3
Josh Cook <joshuacook0023@gmail.com>
2021-06-14 08:36:10 -0400

    feat: basic deep hierarchical structure of Sp7

---
+++ src/models/speclet_seven.py
@@ -0,0 +1,198 @@
+"""Speclet Model Six."""
+
+from pathlib import Path
+from typing import Optional, Tuple
+
+import pymc3 as pm
+from theano import shared as ts
+from theano import tensor
+
+from src.data_processing import achilles as achelp
+from src.loggers import logger
+from src.managers.model_data_managers import CrcDataManager, DataManager
+from src.models.speclet_model import ReplacementsDict, SpecletModel
+
+
+class SpecletSeven(SpecletModel):
+    """SpecletSeven Model.
+
+    $$
+    \\begin{aligned}
+    lfc &\\sim N(\\mu, \\sigma) \\\\
+    \\mu &= a_{s,c} \\quad \\sigma \\sim HN(1) \\\\
+    a_{s,c} &\\sim N(\\mu_a, \\sigma_a)_{s,c} \\\\
+    \\mu_a &\\sim N(\\mu_{\\mu_a}, \\sigma_{\\mu_a})_{g,c}
+      \\quad \\sigma_a \\sim HN(\\sigma_{\\sigma_a})_{s}
+      \\quad \\sigma_{\\sigma_a} \\sim HN(1) \\\\
+    \\mu_{\\mu_a} &\\sim N(\\mu_{\\mu_{\\mu_a}}, \\sigma_{\\mu_{\\mu_a}})_{g,l}
+      \\quad \\sigma_{\\mu_a} \\sim HN(\\sigma_{\\sigma_{\\mu_a}})_{c}
+      \\quad \\sigma_{\\sigma_{\\mu_a}} \\sim HN(1)_{c} \\\\
+    \\mu_{\\mu_{\\mu_a}} &\\sim N(0, 1)
+      \\quad \\sigma_{\\mu_{\\mu_a}} \\sim HN(1)
+    \\end{aligned}
+    $$
+
+    where:
+
+    - s: sgRNA
+    - g: gene
+    - c: cell line
+    - l: cell line lineage
+    """
+
+    def __init__(
+        self,
+        name: str,
+        root_cache_dir: Optional[Path] = None,
+        debug: bool = False,
+        data_manager: Optional[DataManager] = None,
+    ) -> None:
+        """Instantiate a SpecletSeven model.
+
+        Args:
+            name (str): A unique identifier for this instance of SpecletSeven. (Used
+              for cache management.)
+            root_cache_dir (Optional[Path], optional): The directory for caching
+              sampling/fitting results. Defaults to None.
+            debug (bool, optional): Are you in debug mode? Defaults to False.
+            data_manager (Optional[DataManager], optional): Object that will manage the
+              data. If None (default), a `CrcDataManager` is created automatically.
+        """
+        logger.debug("Instantiating a SpecletSeven model.")
+        if data_manager is None:
+            logger.debug("Creating a data manager since none was supplied.")
+            data_manager = CrcDataManager(debug=debug)
+
+        super().__init__(
+            name="speclet-six_" + name,
+            root_cache_dir=root_cache_dir,
+            debug=debug,
+            data_manager=data_manager,
+        )
+
+    def model_specification(self) -> Tuple[pm.Model, str]:
+        """Build SpecletSeven model.
+
+        Returns:
+            Tuple[pm.Model, str]: The model and name of the observed variable.
+        """
+        logger.info("Beginning PyMC3 model specification.")
+        data = self.data_manager.get_data()
+
+        total_size = data.shape[0]
+        co_idx = achelp.common_indices(data)
+
+        # Shared Theano variables
+        logger.info("Getting Theano shared variables.")
+        sgrna_idx_shared = ts(co_idx.sgrna_idx)
+        sgrna_to_gene_idx_shared = ts(co_idx.sgrna_to_gene_idx)
+        cellline_idx_shared = ts(co_idx.cellline_idx)
+        cellline_to_lineage_idx_shared = ts(co_idx.cellline_to_lineage_idx)
+        lfc_shared = ts(data.lfc.values)
+
+        self.shared_vars = {
+            "sgrna_idx_shared": sgrna_idx_shared,
+            "sgrna_to_gene_idx_shared": sgrna_to_gene_idx_shared,
+            "cellline_idx_shared": cellline_idx_shared,
+            "cellline_to_lineage_idx_shared": cellline_to_lineage_idx_shared,
+            "lfc_shared": lfc_shared,
+        }
+
+        logger.info("Creating PyMC3 model for SpecletSeven.")
+        logger.warning(
+            "Still need to implement varying effect for source on batch effect."
+        )
+
+        with pm.Model() as model:
+            μ_μ_μ_a = pm.Normal("μ_μ_μ_a", 0, 1)
+            σ_μ_μ_a = pm.HalfNormal("σ_μ_μ_a", 1)
+
+            μ_μ_a_offset = pm.Normal(
+                "μ_μ_a_offset", 0, 1, shape=(co_idx.n_genes, co_idx.n_lineages)
+            )
+            μ_μ_a = pm.Deterministic("μ_μ_a", μ_μ_μ_a + μ_μ_a_offset * σ_μ_μ_a)
+            σ_σ_μ_a = pm.HalfNormal("σ_σ_μ_a", 1)
+            σ_μ_a = pm.HalfNormal("σ_μ_a", σ_σ_μ_a, shape=(1, co_idx.n_celllines))
+
+            μ_a_offset = pm.Normal(
+                "μ_a_offset", 0, 1, shape=(co_idx.n_genes, co_idx.n_celllines)
+            )
+            μ_a = pm.Deterministic(
+                "μ_a",
+                μ_μ_a[:, cellline_to_lineage_idx_shared]
+                + μ_a_offset
+                * tensor.ones(shape=(co_idx.n_genes, co_idx.n_celllines))
+                * σ_μ_a,
+            )
+            σ_σ_a = pm.HalfNormal("σ_σ_a", 1)
+            σ_a = pm.HalfNormal("σ_a", σ_σ_a, shape=(co_idx.n_sgrnas, 1))
+            a_offset = pm.Normal(
+                "a_offset", 0, 1, shape=(co_idx.n_sgrnas, co_idx.n_celllines)
+            )
+            a = pm.Deterministic("a", μ_a[sgrna_to_gene_idx_shared, :] + a_offset * σ_a)
+
+            μ = pm.Deterministic("μ", a[sgrna_idx_shared, cellline_idx_shared])
+
+            # Standard deviation of log-fold change, varies per batch.
+            σ = pm.HalfNormal("σ", 1)
+
+            lfc = pm.Normal(  # noqa: F841
+                "lfc",
+                μ,
+                σ,
+                observed=lfc_shared,
+                total_size=total_size,
+            )
+
+        logger.debug("Finished building model.")
+
+        return model, "lfc"
+
+    def get_replacement_parameters(self) -> ReplacementsDict:
+        """Make a dictionary mapping the shared data variables to new data.
+
+        Raises:
+            AttributeError: Raised if there is no data manager.
+            AttributeError: Raised if there are no shared variables.
+
+        Returns:
+            ReplacementsDict: A dictionary mapping new data to shared variables.
+        """
+        logger.debug("Making dictionary of replacement parameters.")
+        if self.data_manager is None:
+            raise AttributeError(
+                "Cannot create replacement parameters without a DataManager."
+            )
+        if self.shared_vars is None:
+            raise AttributeError(
+                "No shared variables - cannot create replacement parameters.."
+            )
+
+        data = self.data_manager.get_data()
+        mb_size = self.data_manager.get_batch_size()
+        co_idx = achelp.common_indices(data)
+
+        sgrna_idx_batch = pm.Minibatch(co_idx.sgrna_idx, batch_size=mb_size)
+        cellline_idx_batch = pm.Minibatch(co_idx.cellline_idx, batch_size=mb_size)
+        lfc_data_batch = pm.Minibatch(data.lfc.values, batch_size=mb_size)
+
+        return {
+            self.shared_vars["sgrna_idx_shared"]: sgrna_idx_batch,
+            self.shared_vars["cellline_idx_shared"]: cellline_idx_batch,
+            self.shared_vars["lfc_shared"]: lfc_data_batch,
+        }
+
+    def update_mcmc_sampling_parameters(self) -> None:
+        """Adjust the ADVI parameters depending on the state of the object."""
+        logger.info("Updating the MCMC sampling parameters.")
+        self.mcmc_sampling_params.draws = 4000
+        self.mcmc_sampling_params.tune = 2000
+        self.mcmc_sampling_params.target_accept = 0.99
+        return None
+
+    def update_advi_sampling_parameters(self) -> None:
+        """Adjust the ADVI parameters depending on the state of the object."""
+        logger.info("Updating the ADVI fitting parameters.")
+        parameter_adjustment_map = {True: 40000, False: 100000}
+        self.advi_sampling_params.n_iterations = parameter_adjustment_map[self.debug]
+        return None
---
+++ tests/models/test_speclet_seven.py
@@ -0,0 +1,65 @@
+from pathlib import Path
+
+import pytest
+
+from src.managers.model_data_managers import CrcDataManager
+from src.models.speclet_seven import SpecletSeven
+
+
+@pytest.fixture(autouse=True)
+def data_manager_use_test_data(monkeypatch: pytest.MonkeyPatch):
+    def mock_get_data_path(*args, **kwargs) -> Path:
+        return Path("tests", "depmap_test_data.csv")
+
+    monkeypatch.setattr(CrcDataManager, "get_data_path", mock_get_data_path)
+
+
+class TestSpecletSeven:
+    @pytest.fixture(scope="function")
+    def sp7(self, tmp_path: Path) -> SpecletSeven:
+        return SpecletSeven("test-model", root_cache_dir=tmp_path)
+
+    def test_init(self, sp7: SpecletSeven):
+        assert sp7.model is None
+        assert sp7.mcmc_results is None
+        assert sp7.advi_results is None
+        assert sp7.data_manager is not None
+
+    def test_build_model_spec(self, sp7: SpecletSeven):
+        sp7.build_model()
+        assert sp7.model is not None
+
+    top_priors = ["μ_μ_μ_a", "σ_μ_μ_a", "σ_σ_μ_a", "σ_σ_a", "σ"]
+
+    @pytest.mark.DEV
+    @pytest.mark.parametrize("fit_method", ["mcmc", "advi"])
+    def test_mcmc_sampling(self, sp7: SpecletSeven, fit_method: str):
+        sp7.build_model()
+        assert sp7.model is not None
+
+        n_draws, n_chains = 10, 1
+
+        if fit_method == "mcmc":
+            fit_res = sp7.mcmc_sample_model(
+                mcmc_draws=n_draws,
+                tune=10,
+                chains=n_chains,
+                cores=n_chains,
+                target_accept=0.8,
+                prior_pred_samples=10,
+                post_pred_samples=10,
+                ignore_cache=True,
+            )
+            assert sp7.mcmc_results is not None
+            assert sp7.advi_results is None
+        else:
+            fit_res, _ = sp7.advi_sample_model(
+                n_iterations=20,
+                draws=n_draws,
+                prior_pred_samples=10,
+                post_pred_samples=10,
+                ignore_cache=True,
+            )
+
+        for p in self.top_priors:
+            assert fit_res.posterior[p].shape == (n_chains, n_draws)
