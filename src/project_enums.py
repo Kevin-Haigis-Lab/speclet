df9d161d61cd17096d57cb2da756756961fd43e2
Josh Cook <joshuacook0023@gmail.com>
2021-06-30 16:43:02 -0400

    test: increase coverage of test_cache_managers.py

--- src/project_enums.py
+++ src/project_enums.py
@@ -1,6 +1,7 @@
 """Project-wide enum classes."""
 
 from enum import Enum
+from typing import NoReturn
 
 
 class ModelFitMethod(str, Enum):
@@ -8,3 +9,18 @@ class ModelFitMethod(str, Enum):
 
     advi = "ADVI"
     mcmc = "MCMC"
+
+
+#### ---- Exhaustiveness checks ---- ####
+
+
+def assert_never(value: NoReturn) -> NoReturn:
+    """Force runtime and static enumeration exhausstiveness.
+
+    Args:
+        value (NoReturn): Some value passed as an enum value.
+
+    Returns:
+        NoReturn: Nothing.
+    """
+    assert False, f"Unhandled value: {value} ({type(value).__name__})"
