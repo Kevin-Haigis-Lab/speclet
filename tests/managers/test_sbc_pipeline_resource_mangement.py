d463e9c41801712b500e0ed0ab5e83b83a0d239e
Josh Cook <joshuacook0023@gmail.com>
2021-06-30 17:50:23 -0400

    test: test enum values and strings for SBC resource manager

--- tests/managers/test_sbc_pipeline_resource_mangement.py
+++ tests/managers/test_sbc_pipeline_resource_mangement.py
@@ -9,7 +9,26 @@ from src.project_enums import ModelFitMethod
 @pytest.mark.parametrize("model", [a.value for a in ModelOption])
 @pytest.mark.parametrize("fit_method", [a.value for a in ModelFitMethod])
 @pytest.mark.parametrize("data_size", [a.value for a in MockDataSizes])
-def test_resources_available_for_models(model: str, fit_method: str, data_size: str):
+def test_resources_available_for_models_string_inputs(
+    model: str, fit_method: str, data_size: str
+):
+    rm = RM(
+        model=model,  # type: ignore
+        name="test-model",
+        mock_data_size=data_size,  # type: ignore
+        fit_method=fit_method,  # type: ignore
+    )
+    assert int(rm.memory) > 0
+    assert rm.time is not None
+    assert rm.cores >= 1
+
+
+@pytest.mark.parametrize("model", ModelOption)
+@pytest.mark.parametrize("fit_method", ModelFitMethod)
+@pytest.mark.parametrize("data_size", MockDataSizes)
+def test_resources_available_for_models(
+    model: ModelOption, fit_method: ModelFitMethod, data_size: MockDataSizes
+):
     rm = RM(
         model=model,  # type: ignore
         name="test-model",
