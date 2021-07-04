b4d2810cf9dd5ff45be4f4aea01c351fbd512b92
Josh Cook <joshuacook0023@gmail.com>
2021-07-04 08:33:59 -0400

    feat: CLI to check a configuration file; added to project-build CI

---
+++ src/command_line_interfaces/check_model_configuration.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""CLI for checking a model configuration file."""
+
+from pathlib import Path
+
+import typer
+
+from src.models import configuration as c
+
+
+def check_model_configuration(path: Path) -> None:
+    """Check a model configuration file.
+
+    Performs the following checks:
+
+    1. Configuration file can be read in and parsed into the data structure.
+    2. All names are unique.
+
+    Args:
+        path (Path): Path to the config file.
+    """
+    typer.echo(f"Checking model config: '{path.as_posix()}'")
+    configs = c.get_model_configurations(path)
+    c.check_model_names_are_unique(configs)
+    typer.secho("Configuration file looks good.", fg=typer.colors.GREEN)
+    return None
+
+
+if __name__ == "__main__":
+    typer.run(check_model_configuration)
