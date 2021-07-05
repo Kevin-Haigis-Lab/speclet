d52c776b8b88dcfd3708ffd874591f45265055d0
Josh Cook <joshuacook0023@gmail.com>
2021-07-05 06:56:03 -0400

    feat: timedelta formatter with specific DRMAA format option

--- src/formatting.py
+++ src/formatting.py
@@ -2,7 +2,7 @@
 
 from datetime import timedelta
 from enum import Enum, unique
-from typing import Union
+from typing import Callable, Dict, Optional, Union
 
 
 @unique
@@ -10,9 +10,18 @@ class TimeDeltaFormat(str, Enum):
     """Pre-built timedelta formatting options."""
 
     SLURM = "{days}-{hours:02d}:{minutes:02d}"
+    DRMAA = "{hours:02d}:{minutes:02d}:00"
 
 
-def format_timedelta(tdelta: timedelta, fmt: Union[str, TimeDeltaFormat]) -> str:
+def _drmaa_timedelta_format_callabck(d: Dict[str, int]) -> None:
+    d["hours"] = (d["days"] * 24) + d["hours"]
+
+
+def format_timedelta(
+    tdelta: timedelta,
+    fmt: Union[str, TimeDeltaFormat],
+    callback: Optional[Callable[[Dict[str, int]], None]] = None,
+) -> str:
     """Format time delta object.
 
     Args:
@@ -24,9 +33,14 @@ def format_timedelta(tdelta: timedelta, fmt: Union[str, TimeDeltaFormat]) -> str
         [type]: Formatted time delta.
     """
     if isinstance(fmt, TimeDeltaFormat):
+        if fmt is TimeDeltaFormat.DRMAA and callback is None:
+            callback = _drmaa_timedelta_format_callabck
+
         fmt = fmt.value
 
     d = {"days": tdelta.days}
     d["hours"], rem = divmod(tdelta.seconds, 3600)
     d["minutes"], d["seconds"] = divmod(rem, 60)
+    if callback is not None:
+        callback(d)
     return fmt.format(**d)
--- tests/test_formatting.py
+++ tests/test_formatting.py
@@ -22,3 +22,21 @@ from src import formatting
 def test_format_timedelta_slurm(input: timedelta, expected: str):
     output = formatting.format_timedelta(input, fmt=formatting.TimeDeltaFormat.SLURM)
     assert output == expected
+
+
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        (timedelta(days=3), "72:00:00"),
+        (timedelta(days=3, hours=4), "76:00:00"),
+        (timedelta(days=3, hours=4, minutes=2), "76:02:00"),
+        (timedelta(days=10, hours=15, minutes=45), "255:45:00"),
+        (timedelta(days=10, hours=15, minutes=45, seconds=59), "255:45:00"),
+        (timedelta(days=100, hours=15, minutes=45), "2415:45:00"),
+        (timedelta(days=0, hours=28), "28:00:00"),
+        (timedelta(days=0, minutes=70), "01:10:00"),
+    ],
+)
+def test_format_timedelta_drmaa(input: timedelta, expected: str):
+    output = formatting.format_timedelta(input, fmt=formatting.TimeDeltaFormat.DRMAA)
+    assert output == expected
