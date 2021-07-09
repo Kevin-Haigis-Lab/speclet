d756e227ec96a934530d656982bcf3cd5c155bcc
Josh Cook <joshuacook0023@gmail.com>
2021-07-09 10:29:18 -0400

    style: rename yml to yaml files

--- setup-env.sh
+++ setup-env.sh
@@ -2,9 +2,9 @@
 
 bash ~/.bashrc
 
-conda create -n speclet_snakemake -f snakemake_environment.yml
+conda create -n speclet_snakemake -f snakemake_environment.yaml
 
-conda create -n speclet -f environment.yml
+conda create -n speclet -f environment.yaml
 conda activate speclet
 
 pre-commit install
