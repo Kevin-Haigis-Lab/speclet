b6e0d78a0426e1760eca88b09b1910d931e53646
Josh Cook <joshuacook0023@gmail.com>
2021-08-16 18:10:06 -0400

    feat: CLI tool to count number of lines of code in dirs

--- src/command_line_interfaces/speclet_cli.py
+++ src/command_line_interfaces/speclet_cli.py
@@ -20,6 +20,8 @@ app = typer.Typer()
 
 set_console_handler_level(logging.ERROR)
 
+#### ---- Make model graph images ---- ####
+
 
 @app.command()
 def model_graphs(
@@ -63,6 +65,79 @@ def model_graphs(
         mdl_graph = pm.model_to_graphviz(sp_model.model)
         mdl_graph.render(output_path.as_posix(), format="pdf", cleanup=True)
     return
+
+
+#### ---- Count number of lines of code ---- ####
+
+
+def _count_lines_in_file(f: Path) -> int:
+    n: int = 0
+    try:
+        with open(f, "r") as file:
+            for line in file:
+                if line.strip():
+                    n += 1
+    except UnicodeDecodeError:
+        typer.secho(
+            f"Warning: unable to read file {f.as_posix()}",
+            fg=typer.colors.RED,
+            err=True,
+        )
+    return n
+
+
+def _recursively_count_lines_in_dir(
+    dir: Path, file_types: Optional[list[str]] = None, ignore_dirs: list[str] = []
+) -> int:
+    n_lines = 0
+    for p in dir.iterdir():
+        if p.name in ignore_dirs:
+            continue
+        if p.is_dir():
+            n_lines += _recursively_count_lines_in_dir(
+                p, file_types=file_types, ignore_dirs=ignore_dirs
+            )
+        elif p.is_file():
+            if (file_types is None) or (p.suffix in file_types):
+                n_lines += _count_lines_in_file(p)
+    return n_lines
+
+
+@app.command()
+def lines_of_code(
+    file_types: list[str] = [".py", ".smk", ".sh", ".zsh", ".R", ".r"],
+    dirs: list[Path] = [
+        Path("src"),
+        Path("pipelines"),
+        Path("tests"),
+        Path("munge"),
+        Path("data"),
+    ],
+    ignore_dirs: list[str] = ["__pycache__", ".ipynb_checkpoints", ".DS_Store"],
+) -> None:
+    """Count the lines of code.
+
+    Args:
+        file_types (list[str], optional): File types to include. Defaults to list of
+          common file types for code.
+        dirs (list[Path], optional): Directories to search recursively.
+        ignore_dirs (list[str], optional): Names of subdirectories to ignore. Defaults
+          to some standard caching and checkpoint directories.
+    """
+    line_counts: dict[Path, int] = {d: -1 for d in dirs}
+    for dir in dirs:
+        if not dir.exists() and dir.is_dir():
+            typer.secho(f"Unable to analyze files in directory '{dir}'")
+            continue
+        line_counts[dir] = _recursively_count_lines_in_dir(
+            dir, file_types=file_types, ignore_dirs=ignore_dirs
+        )
+
+    typer.secho("Lines of code:", fg=typer.colors.BRIGHT_BLUE)
+    for dir, n_lines in line_counts.items():
+        typer.secho(
+            f"  {dir.as_posix().ljust(10)} -  {n_lines:,}", fg=typer.colors.BLUE
+        )
 
 
 if __name__ == "__main__":
