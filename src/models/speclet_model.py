d70ff622abc7b9092c8593eb7cf213b9d3fe73ba
jhrcook <joshuacook0023@gmail.com>
2021-09-01 11:52:38 -0400

    fix: SBC not using update modeling data

--- src/models/speclet_model.py
+++ src/models/speclet_model.py
@@ -467,6 +467,7 @@ class SpecletModel:
         if mock_data is not None:
             logger.info("Setting provided mock data as data.")
             self.data_manager.set_data(mock_data)
+            mock_data = self.data_manager.data  # Get result of any transformations.
         elif size is not None:
             logger.info("Creating new simulation data.")
             mock_data = self.data_manager.generate_mock_data(
@@ -486,8 +487,10 @@ class SpecletModel:
         with self.model:
             priors = pm.sample_prior_predictive(samples=1, random_seed=random_seed)
 
+        logger.info("Updating observed variable with generated data.")
         mock_data[self.observed_var_name] = priors.get(self.observed_var_name).flatten()
         self.data_manager.set_data(mock_data, apply_transformations=False)
+        logger.info("Saving mock data to SBC cache.")
         sbc_fm.save_sbc_data(mock_data)
 
         # Update shared variable with adjusted observed data.
