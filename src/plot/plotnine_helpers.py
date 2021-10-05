3a33ca10faf23e84cbfab755006b6a789460a4ea
Josh Cook <joshuacook0023@gmail.com>
2021-10-05 18:10:12 -0400

    feat: function to set plotnine theme

--- src/plot/plotnine_helpers.py
+++ src/plot/plotnine_helpers.py
@@ -3,13 +3,15 @@
 from enum import Enum
 from typing import Union
 
+import plotnine as gg
+
 
 class PlotnineUnits(str, Enum):
     """Units used in Plotnine."""
 
-    pt = "pt"
-    lines = "lines"
-    inches = "in"
+    PT = "pt"
+    LINES = "lines"
+    INCHES = "in"
 
 
 def margin(
@@ -17,7 +19,7 @@ def margin(
     b: float = 0,
     l: float = 0,  # noqa: E741
     r: float = 0,
-    units: PlotnineUnits = PlotnineUnits.pt,
+    units: Union[str, PlotnineUnits] = PlotnineUnits.PT,
 ) -> dict[str, Union[float, str]]:
     """Return a dictionary of margin data.
 
@@ -26,11 +28,28 @@ def margin(
         b (float, optional): Bottom margin. Defaults to 0.
         l (float, optional): Left margin. Defaults to 0.
         r (float, optional): Right margin. Defaults to 0.
-        units (str, optional): Units for the margin. Defaults to "pt".
+        units (Union[str, PlotnineUnits], optional): Units for the margin. Defaults to
+          "PT".
 
     Returns:
         Dict[str, Any]: A dictionary for use as margin data in plotnine plots.
     """
     if isinstance(units, str):
         units = PlotnineUnits(units)
-    return {"t": t, "b": b, "l": l, "r": r, "units": units}
+    return {"t": t, "b": b, "l": l, "r": r, "units": units.value}
+
+
+def set_gg_theme(figure_size: tuple[float, float] = (4, 4)) -> None:
+    """Set the default plotnine theme.
+
+    Args:
+        figure_size (tuple[float, float], optional): Figure size. Defaults to (4, 4).
+    """
+    gg.theme_set(
+        gg.theme_classic()
+        + gg.theme(
+            figure_size=figure_size,
+            axis_ticks_major=gg.element_blank(),
+            strip_background=gg.element_blank(),
+        )
+    )
