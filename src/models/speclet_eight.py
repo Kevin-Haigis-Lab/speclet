11eb51f3197d5d1f78c603eb62346ba5e6fc9aa5
Josh Cook <joshuacook0023@gmail.com>
2021-10-15 10:27:00 -0400

    feat Sp8 with varying intercept and RNA expr covariate

--- src/models/speclet_eight.py
+++ src/models/speclet_eight.py
@@ -2,10 +2,14 @@
 
 
 from pathlib import Path
-from typing import Optional
+from typing import Final, Optional, Union
 
+import janitor  # noqa: F401
+import numpy as np
+import pandas as pd
 import pymc3 as pm
 from pydantic import BaseModel
+from pymc3.model import FreeRV as pmFreeRV
 from theano import shared as ts
 from theano import tensor as tt
 
@@ -37,6 +41,21 @@ def _append_total_read_counts(df: Data) -> Data:
 
 def _add_useful_read_count_columns(df: Data) -> Data:
     return achelp.add_useful_read_count_columns(df)
+
+
+def _thin_data_columns(df: Data) -> Data:
+    keep_cols: Final[list[str]] = [
+        "sgrna",
+        "hugo_symbol",
+        "depmap_id",
+        "lineage",
+        "counts_initial_adj",
+        "counts_final",
+        "rna_expr_gene_lineage",
+        "p_dna_batch",
+        "screen",
+    ]
+    return df[keep_cols]
 
 
 class SpecletEight(SpecletModel):
@@ -85,10 +104,10 @@ class SpecletEight(SpecletModel):
         if self._config.broad_only:
             data_transformations.append(achelp.filter_for_broad_source_only)
 
-        # Need to add at end because `p_dna_batch` becomes non-categorical.
-        data_transformations.append(achelp.set_achilles_categorical_columns)
-
         data_manager.add_transformation(data_transformations)
+        data_manager.add_transformation(_thin_data_columns)
+        # Need to add at end because `p_dna_batch` becomes non-categorical.
+        data_manager.add_transformation(achelp.set_achilles_categorical_columns)
 
         super().__init__(name, data_manager, root_cache_dir=root_cache_dir, debug=debug)
 
@@ -122,47 +141,42 @@ class SpecletEight(SpecletModel):
             "batch": data.p_dna_batch.cat.categories,
         }
 
+        rna_expr_matrix = _rna_expr_gene_lineage_matrix(data, "rna_expr_gene_lineage")
+        _sgrna_by_cell_line = (co_idx.n_sgrnas, co_idx.n_celllines)
+
         with pm.Model(coords=coords) as model:
             s = pm.Data("sgrna_idx", co_idx.sgrna_idx)
             g_s = pm.Data("sgrna_to_gene_idx", co_idx.sgrna_to_gene_idx)
             c = pm.Data("cell_line_idx", co_idx.cellline_idx)
-            # l_c = pm.Data("cell_line_to_lineage_idx", co_idx.cellline_to_lineage_idx)
+            l_c = pm.Data("cell_line_to_lineage_idx", co_idx.cellline_to_lineage_idx)
             # b = pm.Data("batch_idx", b_idx.batch_idx)
+            rna_expr = pm.Data("rna_expression_gl", rna_expr_matrix)
             x_initial = pm.Data("x_initial", data.counts_initial_adj.values)
             counts_final = pm.Data("counts_final", data.counts_final.values)
 
-            mu_mu_h = pm.Normal("mu_mu_h", 0, 1)
-            sigma_mu_h = pm.HalfNormal("sigma_mu_h", 1)
-            mu_h = pm.Normal("mu_h", mu_mu_h, sigma_mu_h, dims=("gene", "lineage"))
-            sigma_sigma_h = pm.HalfNormal("sigma_sigma_h", 2.5)
-            sigma_h = pm.HalfNormal("sigma_h", sigma_sigma_h, dims="cell_line")
-            h = pm.Normal(
-                "h",
-                mu_h,
-                tt.ones(shape=(co_idx.n_genes, co_idx.n_celllines)) * sigma_h,
-                dims=("gene", "cell_line"),
+            h = _gene_by_cell_line_hierarchical_structure(
+                "h", co_idx=co_idx, l_c_idx=l_c
+            )
+            q = _gene_by_cell_line_hierarchical_structure(
+                "q", co_idx=co_idx, l_c_idx=l_c
             )
 
-            mu_beta_sgrna = pm.Deterministic("mu_beta_sgrna", h[g_s, :])
-            sigma_sigma_beta_sgrna = pm.HalfNormal("sigma_sigma_beta_sgrna", 2.5)
-            sigma_beta_sgrna = pm.HalfNormal(
-                "sigma_beta_sgrna", sigma_sigma_beta_sgrna, dims="sgrna"
-            )
+            mu_beta = pm.Deterministic(
+                "mu_beta", h + q * rna_expr
+            )  # shape: [gene x cell line]
+
+            sigma_sigma_beta = pm.HalfNormal("sigma_sigma_beta", 1)
+            sigma_beta = pm.HalfNormal("sigma_beta", sigma_sigma_beta, dims="gene")
+
             beta_sgrna = pm.Normal(
                 "beta_sgrna",
-                mu_beta_sgrna,
-                sigma_beta_sgrna.reshape((-1, 1))
-                * tt.ones(shape=(co_idx.n_sgrnas, co_idx.n_celllines)),
+                mu_beta[g_s, :],
+                tt.ones(shape=_sgrna_by_cell_line)
+                * sigma_beta[g_s].reshape((co_idx.n_sgrnas, 1)),
                 dims=("sgrna", "cell_line"),
             )
 
-            # mu_beta_batch = pm.Normal("mu_beta_batch", 0, 0.5)
-            # sigma_beta_batch = pm.HalfCauchy("sigma_beta_batch", 1)
-            # beta_batch = pm.Normal(
-            #     "beta_batch", mu_beta_batch, sigma_beta_batch, dims=("batch")
-            # )
-
-            eta = pm.Deterministic("eta", beta_sgrna[s, c])  # + beta_batch[b])
+            eta = pm.Deterministic("eta", beta_sgrna[s, c])
             mu = pm.Deterministic("mu", pm.math.exp(eta))
             alpha = pm.HalfCauchy("alpha", 5)
             y = pm.NegativeBinomial(  # noqa: F841
@@ -170,3 +184,45 @@ class SpecletEight(SpecletModel):
             )
 
         return model, "y"
+
+
+def _gene_by_cell_line_hierarchical_structure(
+    name: str, co_idx: achelp.CommonIndices, l_c_idx: Union[np.ndarray, pm.Data]
+) -> pmFreeRV:
+    _gene_by_cell_line = (co_idx.n_genes, co_idx.n_celllines)
+
+    mu_mu_beta = pm.Normal(f"mu_mu_{name}", 0, 1)
+    sigma_mu_beta = pm.HalfNormal(f"sigma_mu_{name}", 1)
+    mu_beta = pm.Normal(
+        f"mu_{name}", mu_mu_beta, sigma_mu_beta, dims=("gene", "lineage")
+    )
+
+    sigma_sigma_beta = pm.HalfNormal(f"sigma_sigma_{name}", 1)
+    sigma_beta = pm.HalfNormal(f"sigma_{name}", sigma_sigma_beta, dims="lineage")
+
+    beta = pm.Normal(
+        name,
+        mu_beta[:, l_c_idx],
+        tt.ones(shape=_gene_by_cell_line) * sigma_beta[l_c_idx],
+        dims=("gene", "cell_line"),
+    )
+    return beta
+
+
+def _rna_expr_gene_lineage_matrix(df: pd.DataFrame, rna_col: str) -> np.ndarray:
+    """Pivot a long data frame into a matrix of RNA expression data.
+
+    Output is a matrix of shape (gene x cell line).
+
+    """
+    pivot_df = (
+        df.copy()[["hugo_symbol", "depmap_id", rna_col]]
+        .drop_duplicates()
+        .sort_values("depmap_id")
+        .pivot_wider(index="hugo_symbol", names_from="depmap_id", values_from=rna_col)
+        .sort_values("hugo_symbol", ascending=True)
+    )
+
+    assert all(pivot_df.hugo_symbol.values == df.hugo_symbol.cat.categories)
+    assert all(pivot_df.columns[1:] == df.depmap_id.cat.categories)
+    return pivot_df.drop(columns="hugo_symbol").values
