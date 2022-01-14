bc479595b83bb06d6a15c45e6a9a63ddb12d12ab
Josh Cook <joshuacook0023@gmail.com>
2022-01-14 10:24:17 -0500

    feat: finish main sampling arguments for dataclass of Stan MCMC

--- speclet/modeling/fitting_arguments.py
+++ speclet/modeling/fitting_arguments.py
@@ -1,9 +1,9 @@
 """Concerning arguments for fitting Bayesian models."""
 
+from importlib.metadata import version
 from typing import Any, Callable, Iterable, Optional, Union
 
 import numpy as np
-import pymc3 as pm
 from pydantic import BaseModel, Field, PositiveInt, confloat
 
 from speclet.types import BasicTypes, VIMethod
@@ -15,20 +15,46 @@ def _randint() -> int:
     return np.random.randint(0, 10000)
 
 
-class StanMCMCSamplingArguments(BaseModel):
-    """MCMC sampling arguments for Stan."""  # TODO: this is incomplete...
+def _check_versions(expected: str, libname: str) -> None:
+    actual_ver = version(libname)
+    if expected != actual_ver:
+        raise NotImplementedError(
+            f"Support for {libname} {expected} -> current ver {actual_ver}"
+        )
+    return None
 
+
+class StanMCMCSamplingArguments(BaseModel):
+    """MCMC sampling arguments for Stan."""
+
+    _pystan_ver: str = "3.3.0"
+    _httpstan_ver: str = "4.6.1"
     num_chains: PositiveInt = 4
     num_samples: PositiveInt = 1000
     num_warmup: PositiveInt = 1000
     save_warmup: bool = False
+    num_thin: int = 1
+    delta: TargetAcceptFloat = 0.8  # type: ignore
+    max_depth: PositiveInt = 10
     random_seed: PositiveInt = Field(default_factory=_randint)
+
+    def __init__(self, **data: dict[str, Any]) -> None:
+        """Create a Pymc3SampleArguments object.
+
+        Raises:
+            NotImplementedError: If the currently installed PyMC3 version is different
+            from the supported version.
+        """
+        super().__init__(**data)
+        _check_versions(self._pystan_ver, "pystan")
+        _check_versions(self._httpstan_ver, "httpstan")
+        return None
 
 
 class Pymc3SampleArguments(BaseModel):
     """Model `sample()` keyword arguments (PyMC3 v3.11.4)."""
 
-    _pymc3_version: str = "3.11.4"
+    _pymc3_ver: str = "3.11.4"
     draws: int = 1000
     step: Optional[Union[Callable, Iterable[Callable]]] = None
     init: str = "auto"
@@ -53,16 +79,14 @@ class Pymc3SampleArguments(BaseModel):
             from the supported version.
         """
         super().__init__(**data)
-        if self._pymc3_version != pm.__version__:
-            raise NotImplementedError(
-                f"Support for {self._pymc3_version} -> current ver {pm.__version__}"
-            )
+        _check_versions(self._pymc3_ver, "pymc3")
+        return None
 
 
 class Pymc3FitArguments(BaseModel):
     """Model `fit()` keyword arguments (PyMC3 v3.11.4)."""
 
-    _pymc3_version: str = "3.11.4"
+    _pymc3_ver: str = "3.11.4"
     n: PositiveInt = 10000
     method: VIMethod = "advi"
     draws: PositiveInt = 1000
@@ -78,10 +102,8 @@ class Pymc3FitArguments(BaseModel):
             from the supported version.
         """
         super().__init__(**data)
-        if self._pymc3_version != pm.__version__:
-            raise NotImplementedError(
-                f"Support for {self._pymc3_version} -> current ver {pm.__version__}"
-            )
+        _check_versions(self._pymc3_ver, "pymc3")
+        return None
 
 
 class ModelingSamplingArguments(BaseModel):
