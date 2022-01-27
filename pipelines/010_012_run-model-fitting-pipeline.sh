2e329e984d3dd035cf5fa8e0e8ac6769e4c47727
Josh Cook <joshuacook0023@gmail.com>
2022-01-27 10:37:36 -0500

    fix: minor adjustments for O2 run

--- pipelines/010_012_run-model-fitting-pipeline.sh
+++ pipelines/010_012_run-model-fitting-pipeline.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #SBATCH --account=park
-#SBATCH -c 1
+#SBATCH -c 2
 #SBATCH -p priority
 #SBATCH -t 1-00:00
-#SBATCH --mem 1G
+#SBATCH --mem 4G
 #SBATCH -o logs/%j_sample-pipeline.log
 #SBATCH -e logs/%j_sample-pipeline.log
 
@@ -17,7 +17,6 @@ conda activate speclet_smk
 
 SNAKEFILE="pipelines/010_010_model-fitting-pipeline.smk"
 
-# Copy original env file and ammend import of speclet project modules
 ENV_PATH="pipelines/default_environment.yaml"
 
 snakemake \
@@ -27,6 +26,7 @@ snakemake \
     --latency-wait 120 \
     --rerun-incomplete \
     --use-conda \
+    --conda-frontend 'mamba' \
     --drmaa " -c {cluster.cores} -p {cluster.partition} --mem={cluster.mem} -t {cluster.time} -o {cluster.out} -e {cluster.err} -J {cluster.J}" \
     --cluster-config pipelines/010_011_smk-config.yaml \
     --keep-going \
