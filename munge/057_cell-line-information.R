1351eec1b64d470bd3ecf8bdc9a5e513e3e5db26
Josh Cook <joshuacook0023@gmail.com>
2022-06-13 17:51:07 -0400

    munge: rule to make lineage-specific data files (untested)

--- munge/057_cell-line-information.R
+++ munge/057_cell-line-information.R
@@ -15,6 +15,7 @@ library(tidyverse)
 modeling_data_file <- snakemake@input[["modeling_df"]]
 # Output
 cell_line_info_file <- snakemake@output[["cell_line_info"]]
+lineage_counts_file <- snakemake@output[["num_lines_per_lineage"]]
 
 
 # --- Load data ---
@@ -25,7 +26,7 @@ data <- as_tibble(data)
 
 # --- DepMap modeling data cell line information ---
 
-data %>%
+cell_line_info <- data %>%
   distinct(
     screen, depmap_id, lineage, lineage_subtype, is_male, primary_or_metastasis, age,
     replicate_id, p_dna_batch,
@@ -36,5 +37,14 @@ data %>%
     replicate_id, p_dna_batch,
     name = "n_genes"
   ) %>%
-  arrange(lineage, lineage_subtype, depmap_id, screen, replicate_id, p_dna_batch) %>%
-  write_csv(cell_line_info_file)
+  arrange(lineage, lineage_subtype, depmap_id, screen, replicate_id, p_dna_batch)
+
+write_csv(cell_line_info, cell_line_info_file)
+
+lineage_counts <- cell_line_info %>%
+  filter(screen == 'broad') %>%
+  distinct(depmap_id, lineage) %>%
+  count(lineage, sort=TRUE)
+
+write_csv(lineage_counts, lineage_counts_file)
+knitr::kable(lineage_counts)
