40e13a2bd1fd99e50feb3b5afdb36ef142bb4299
jhrcook <joshuacook0023@gmail.com>
2022-07-09 17:55:10 -0400

    feat: updates to conda env, pipeline config, GPU expt. script

--- expt-gpu-run.sh
+++ expt-gpu-run.sh
@@ -2,28 +2,35 @@
 
 #SBATCH --job-name=expt-gpu
 #SBATCH --account=park
-#SBATCH -c 4
-#SBATCH -t 0-12:00
+#SBATCH -c 1
+#SBATCH -t 0-00:10
 #SBATCH -p gpu_quad
 #SBATCH --gres=gpu:rtx8000:1
-#SBATCH --mem 64G
+#SBATCH --mem 16G
 #SBATCH -o logs/%j_expt-gpu.log
 #SBATCH -e logs/%j_expt-gpu.log
 
-module load gcc/9.2.0 cuda/11.2 conda2
+# module load gcc/9.2.0 cuda/11.2 conda2
+module load conda2
 
 # shellcheck source=/dev/null
 source "$HOME/.bashrc"
-conda activate
+conda activate speclet
+
+# Some NVIDIA data.
+which nvidia-smi
+which nvcc
+nvidia-smi
+nvcc --version
 
 # The "job_gpu_monitor.sh" script is provided by RC to help monitor GPU usage.
 /n/cluster/bin/job_gpu_monitor.sh & ./speclet/cli/fit_bayesian_model_cli.py \
     "hnb-single-lineage-prostate" \
     models/model-configs.yaml \
     "PYMC_NUMPYRO" \
-    temp/ \
+    temp/gpu-expt/ \
     --mcmc-chains 1 \
-    --mcmc-cores 2 \
+    --mcmc-cores 1 \
     --seed 123 \
     --broad-only
 
